0397d6f4efbbff3c91ba29c405286eb264a600f0
robertodelgado1999-cloud <roberto.delgado1999@alumnos.udg.mx>
2026-02-13 09:51:03 -0600

    Add files via upload

---
+++ 13FEBRERO/rectificacion_primo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int numero;
+    printf("Dame un numero:\n");
+    scanf("%d",&numero);
+    if(numero==2){
+        printf("Tu numero es primo");
+    }
+    else if(numero%2==0 && numero%1==0 || numero%3==0 || numero%5==0){
+        printf("Tu numero no es primo\n");
+    }
+    else {
+         printf("Tu numero es primo\n");
+    }
+    return 0;
+}
