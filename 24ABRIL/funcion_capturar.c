bea6b76c842caa3b2d73974f4ed3bdfbfa07e9e5
robertodelgado1999-cloud <roberto.delgado1999@alumnos.udg.mx>
2026-04-24 08:36:03 -0600

    Add files via upload

---
+++ 24ABRIL/funcion_capturar.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+void capturar(int numeros[3][3]);
+void mostrar(int numeros[3][3]);
+int diagonal(int numeros[3][3]);
+void archivo(int numeros[3][3],int suma);
+
+int main(){
+    int numeros[3][3];
+    int suma = 0;
+    capturar(numeros);
+    printf("\nLos numeros ingresados son:\n");
+    mostrar(numeros);
+    suma = diagonal(numeros);
+    printf("\nLa suma de la diagonal es: %d\n",suma);
+    archivo(numeros,suma);
+
+}
+
+void capturar(int numeros[3][3]){
+    int i,j;
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            printf("Dame un numero:");
+            scanf("%d",&numeros[i][j]);
+        }
+    }
+}
+
+void mostrar(int numeros[3][3]){
+    int i,j;
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            printf("%d ",numeros[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int diagonal(int numeros[3][3]){
+    int i,suma = 0;
+    for(i=0; i<3; i++){
+        suma += numeros[i][i];
+    }
+    return suma;
+}
+
+void archivo(int numeros[3][3],int suma){
+    int i,j;
+    FILE *f;
+    f = fopen("tablero.txt","w");
+    fprintf(f,("El tablero es:\n"));
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            fprintf(f,"%d ",numeros[i][j]);
+        }
+        fprintf(f,"\n");
+    }
+    fprintf(f,"La suma de la diagonal: %d",suma);
+    fclose(f);
+}
+
